de395e4809541048010dd21cced60b5aa4c5fe89
Gabriel Bergmann <gabrielbergmann10@hotmail.com>
2020-02-07 18:00:51 -0300

    Ocasionando erro

--- app/routes/frm_inclusao_noticia.js
+++ app/routes/frm_inclusao_noticia.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
+    const valor = 1;
     app.get('/formulario_inclusao_noticia', function(req, res){
         res.render("admin/form_add_noticia");
+        valor = 2;
     });
 };
